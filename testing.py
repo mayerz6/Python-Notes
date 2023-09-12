0482f1e35fd815f4b79d8e7b36a8119d3aa1faab
Larry Mayers <info@larrymayers.site>
2023-09-11 23:29:40 -0400

    Revision of base terminology 🚀

--- testing.py
+++ testing.py
@@ -10,5 +10,22 @@ class Dataset:
         print(f"We have updated the version for {self.lang} from version {self.version} to {new_version}")
         self.version = new_version
         
-rec1 = Dataset()
-rec1.upgrade()
+# rec1 = Dataset()
+# rec1.upgrade()
+
+class DB:
+    def __inti__(self, *args, **kwargs):
+        self.status = "inactive"
+        self.connect = None
+        self.records = ""
+        
+    def getConnection(self):
+        print(f'Successfully connected!')
+    def fetchData(self):
+        print(f'Records retrieved from backend database')
+
+time = DB()
+time.getConnection()
+time.fetchData()
+
+ ## print(text1 + ' ' + text2)
